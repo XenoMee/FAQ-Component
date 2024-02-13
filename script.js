cc344ca0794ded28dd925ba6931553e63a71e7cb
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-13 09:46:46 +0200

    add no-display class to paragraph elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const expandButtons = document.querySelectorAll(".btn");
+let isOpen = false;
+
+for(let i = 0; i < expandButtons.length; i++){
+    expandButtons[i].addEventListener("click", function(){
+        
+    })
+}
