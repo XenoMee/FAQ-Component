2c33e2a8df8674c05189cd4df9b43408eb868cbb
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-13 17:57:19 +0200

    rename no-display class to 'collapse'

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 const expandButtons = document.querySelectorAll(".btn");
+const textElements = document.querySelectorAll(".no-display");
 let isOpen = false;
+let isClose = true;
 
 for(let i = 0; i < expandButtons.length; i++){
     expandButtons[i].addEventListener("click", function(){
-        
+        for(let j = 0; j < textElements.length; j++){
+            
+            if(isClose){
+                textElements[j].classList.remove("no-display");
+                isOpen = true;
+                isClose = false;
+            }
+            else {
+                textElements[j].classList.add("no-display");
+                isOpen = false;
+                isClose = true;
+            }
+        }
     })
 }
+
+// const expandButtonEl = document.getElementById("expand-btn");
+// const questionAnswerEl = document.getElementById("question-answer");
+// expandButtonEl.addEventListener('click', function(){
+//     if(isClose){
+//         questionAnswerEl.classList.remove("no-display");
+//         isOpen = true;
+//         isClose = false;
+//     }
+//     else{
+//         questionAnswerEl.classList.add("no-display");
+//         isOpen = false;
+//         isClose = true;
+//     }
+// });
+
+// function checkState(){
+//     if(isClose){
+//         questionAnswerEl.classList.remove("no-display");
+//         isOpen = true;
+//         isClose = false;
+//     }
+//     else{
+//         questionAnswerEl.classList.add("no-display");
+//         isOpen = false;
+//         isClose = true;
+//     }
+// }
+// TODO: Add event listener on button to listen for click events
+// TODO: Expand the question item paragraph
