66bb07beb5ca44a45d75f5ce5aa82dd8bdcd203b
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-13 19:58:49 +0200

    toggle accordion body text

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
 const expandButtons = document.querySelectorAll(".btn");
-const questionAnswerEl = document.querySelectorAll("");
+const questionAnswerEl = document.querySelectorAll(".accordion-collapse");
 let isOpen = false;
 let isClose = true;
 
 for(let i = 0; i < expandButtons.length; i++){
     expandButtons[i].addEventListener("click", function(){
-        this.classList.toggle('open');
+        questionAnswerEl[i].classList.toggle("open");
     })
 }
-
-// const expandButtonEl = document.getElementById("expand-btn");
-// const questionAnswerEl = document.getElementById("question-answer");
-// expandButtonEl.addEventListener('click', function(){
-//     if(isClose){
-//         questionAnswerEl.classList.remove("no-display");
-//         isOpen = true;
-//         isClose = false;
-//     }
-//     else{
-//         questionAnswerEl.classList.add("no-display");
-//         isOpen = false;
-//         isClose = true;
-//     }
-// });
-
-// function checkState(){
-//     if(isClose){
-//         questionAnswerEl.classList.remove("no-display");
-//         isOpen = true;
-//         isClose = false;
-//     }
-//     else{
-//         questionAnswerEl.classList.add("no-display");
-//         isOpen = false;
-//         isClose = true;
-//     }
-// }
-// TODO: Add event listener on button to listen for click events
-// TODO: Expand the question item 
