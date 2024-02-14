829e3b50aec0b6537674eef5cb04ce073adb27fd
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-14 17:10:32 +0200

    store all buttons and accordion content to their own variable

---
+++ script.js
@@ -0,0 +1,17 @@
+const accordionButtons = document.querySelectorAll(".accordion-trigger");
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+// for(let i = 0; i < accordionButtons.length; i++){
+//     accordionButtons[i].addEventListener("click", function(){
+//         if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
+//             accordionButtons[i].setAttribute("aria-expanded", true);
+//             accordionButtons[i].children[1].style.display = "none";
+//             accordionContent[i].setAttribute("aria-hidden", false);
+//         }
+//         else{
+//             accordionButtons[i].setAttribute("aria-expanded", false);
+//             accordionContent[i].setAttribute("aria-hidden", true);
+//             accordionButtons[i].children[1].style.display = "block";
+//         }
+//     });
+// }
