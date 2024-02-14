606ea16c462dab19a90b29794cbdaacf3b3cab6b
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-14 09:52:37 +0200

    remove boolean variables

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const expandButtons = document.querySelectorAll(".btn");
 const questionAnswerEl = document.querySelectorAll(".accordion-collapse");
-let isOpen = false;
-let isClose = true;
 
 for(let i = 0; i < expandButtons.length; i++){
     expandButtons[i].addEventListener("click", function(){
