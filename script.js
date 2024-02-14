1905fd270d109345e7446d0f7059178ce51423da
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-14 17:12:49 +0200

    update button's aria expanded attribute value to true on click if it was false

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const accordionContent = document.querySelectorAll(".accordion-content");
 
 for(let i = 0; i < accordionButtons.length; i++){
     accordionButtons[i].addEventListener("click", function(){
-//         if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
-//             accordionButtons[i].setAttribute("aria-expanded", true);
-//             accordionButtons[i].children[1].style.display = "none";
-//             accordionContent[i].setAttribute("aria-hidden", false);
-//         }
+        if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
+            accordionButtons[i].setAttribute("aria-expanded", true);
+            // accordionButtons[i].children[1].style.display = "none";
+            // accordionContent[i].setAttribute("aria-hidden", false);
+        }
 //         else{
 //             accordionButtons[i].setAttribute("aria-expanded", false);
 //             accordionContent[i].setAttribute("aria-hidden", true);
