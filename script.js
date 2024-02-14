2ce5110c14d09fe72ba66bb3c928c85295d13985
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-14 17:11:12 +0200

    add on click event listener to each button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const accordionButtons = document.querySelectorAll(".accordion-trigger");
 const accordionContent = document.querySelectorAll(".accordion-content");
 
-// for(let i = 0; i < accordionButtons.length; i++){
-//     accordionButtons[i].addEventListener("click", function(){
+for(let i = 0; i < accordionButtons.length; i++){
+    accordionButtons[i].addEventListener("click", function(){
 //         if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
 //             accordionButtons[i].setAttribute("aria-expanded", true);
 //             accordionButtons[i].children[1].style.display = "none";
@@ -13,5 +13,5 @@ const accordionContent = document.querySelectorAll(".accordion-content");
 //             accordionContent[i].setAttribute("aria-hidden", true);
 //             accordionButtons[i].children[1].style.display = "block";
 //         }
-//     });
-// }
+    });
+}
