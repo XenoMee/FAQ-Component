bcfddcdd6342bc133e5f6560a98b1be833c199a7
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-15 16:31:33 +0200

    remove timeout function from script file

--- script.js
+++ script.js
@@ -2,27 +2,22 @@ const accordionButtons = document.querySelectorAll(".accordion-trigger");
 const accordionContent = document.querySelectorAll(".accordion-content");
 const accordionQuestionItem = document.querySelectorAll(".accordion-question");
 
-
-for(let i = 0; i < accordionButtons.length; i++){
-    accordionButtons[i].addEventListener("click", function(){
-        if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
-            // Accordion buttons
-            accordionButtons[i].setAttribute("aria-expanded", true);
-            accordionButtons[i].children[1].style.display = "none";
-            // Accordion Content
-            accordionContent[i].setAttribute("aria-hidden", false);
-            accordionContent[i].style.height = "100%";
-        }
-        else{
-            
-            // Accordion Buttons
-            accordionButtons[i].setAttribute("aria-expanded", false);
-            accordionButtons[i].children[1].style.display = "block";
-            // Accordion Content
-            accordionContent[i].setAttribute("aria-hidden", true);
-            setTimeout(() => {
-            }, 460);
-            accordionContent[i].style.height = "";
-        }
-    });
+for (let i = 0; i < accordionButtons.length; i++) {
+  accordionButtons[i].addEventListener("click", function () {
+    if (accordionButtons[i].getAttribute("aria-expanded") === "false") {
+      // Accordion buttons
+      accordionButtons[i].setAttribute("aria-expanded", true);
+      accordionButtons[i].children[1].style.display = "none";
+      // Accordion Content
+      accordionContent[i].setAttribute("aria-hidden", false);
+      accordionContent[i].style.height = "100%";
+    } else {
+      // Accordion Buttons
+      accordionButtons[i].setAttribute("aria-expanded", false);
+      accordionButtons[i].children[1].style.display = "block";
+      // Accordion Content
+      accordionContent[i].setAttribute("aria-hidden", true);
+      accordionContent[i].style.height = "";
+    }
+  });
 }
