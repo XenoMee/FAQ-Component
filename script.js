e90d68accccaa41d043963ff74e82366bf7c455b
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-15 12:24:55 +0200

    set max-height transition for accordion question items

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const accordionButtons = document.querySelectorAll(".accordion-trigger");
 const accordionContent = document.querySelectorAll(".accordion-content");
 const accordionQuestionItem = document.querySelectorAll(".accordion-question");
 
+
 for(let i = 0; i < accordionButtons.length; i++){
     accordionButtons[i].addEventListener("click", function(){
         if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
             accordionQuestionItem[i].classList.add("grid-flow");
+            accordionQuestionItem[i].style.maxheight = "5rem";
             // Accordion buttons
             accordionButtons[i].setAttribute("aria-expanded", true);
             accordionButtons[i].children[1].style.display = "none";
             // Accordion Content
             accordionContent[i].setAttribute("aria-hidden", false);
-            accordionContent[i].style.height = "100%";
+            accordionContent[i].style.height = "6rem";
         }
         else{
             
