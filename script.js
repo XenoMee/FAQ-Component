a91d43cc84a2df2038796493e36fa05c24e8177c
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-15 10:03:54 +0200

    increase accordion content's height on button click if aria expanded value is false

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const accordionButtons = document.querySelectorAll(".accordion-trigger");
 const accordionContent = document.querySelectorAll(".accordion-content");
+// const accordionQuestionItem = document.querySelectorAll(".accordion-question");
 
 for(let i = 0; i < accordionButtons.length; i++){
     accordionButtons[i].addEventListener("click", function(){
         if(accordionButtons[i].getAttribute("aria-expanded") === "false"){
+            // accordionQuestionItem[i].classList.add("grid-flow");
+            // Accordion buttons
             accordionButtons[i].setAttribute("aria-expanded", true);
             accordionButtons[i].children[1].style.display = "none";
+            // Accordion Content
             accordionContent[i].setAttribute("aria-hidden", false);
+            accordionContent[i].style.height = "100%";
         }
         else{
+            // accordionQuestionItem[i].classList.remove("grid-flow");
+            // Accordion Buttons
             accordionButtons[i].setAttribute("aria-expanded", false);
-            accordionContent[i].setAttribute("aria-hidden", true);
             accordionButtons[i].children[1].style.display = "block";
+            // Accordion Content
+            accordionContent[i].setAttribute("aria-hidden", true);
+            // accordionContent[i].style.height = "0";
         }
     });
 }
