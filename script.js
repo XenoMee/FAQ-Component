60a398f2201ca9dad223f816d8cca2d1e4092a1a
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-15 21:46:20 +0200

    other solution

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-const accordionButtons = document.querySelectorAll(".accordion-trigger");
-const accordionContent = document.querySelectorAll(".accordion-content");
-const accordionQuestionItem = document.querySelectorAll(".accordion-question");
+const accordion = document.querySelector("ul");
 
-for (let i = 0; i < accordionButtons.length; i++) {
-  accordionButtons[i].addEventListener("click", function () {
-    if (accordionButtons[i].getAttribute("aria-expanded") === "false") {
-      // Accordion buttons
-      accordionButtons[i].setAttribute("aria-expanded", true);
-      accordionButtons[i].children[1].style.display = "none";
-      // Accordion Content
-      accordionContent[i].setAttribute("aria-hidden", false);
-      accordionContent[i].style.height = "100%";
-    } else {
-      // Accordion Buttons
-      accordionButtons[i].setAttribute("aria-expanded", false);
-      accordionButtons[i].children[1].style.display = "block";
-      // Accordion Content
-      accordionContent[i].setAttribute("aria-hidden", true);
-      accordionContent[i].style.height = "";
-    }
-  });
+accordion.addEventListener("click", function (e) {
+  const activePanel = e.target.closest(".accordion-question");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
+
+function toggleAccordion(panelToActivate) {
+  const button = panelToActivate.querySelector(".accordion-trigger");
+  const content = panelToActivate.querySelector(".accordion-content");
+
+  if (button.getAttribute("aria-expanded") === "false") {
+    button.setAttribute("aria-expanded", "true");
+    button.children[1].style.display = "none";
+    content.setAttribute("aria-hidden", "false");
+    content.style.height = "100%";
+  } else {
+    button.setAttribute("aria-expanded", "false");
+    button.children[1].style.display = "block";
+    content.setAttribute("aria-hidden", "true");
+    content.style.height = "0%";
+  }
 }
+
+// for (let i = 0; i < accordionButtons.length; i++) {
+//   accordionButtons[i].addEventListener("click", function () {
+//     if (accordionButtons[i].getAttribute("aria-expanded") === "false") {
+//       // Accordion buttons
+//       accordionButtons[i].setAttribute("aria-expanded", true);
+//       accordionButtons[i].children[1].style.display = "none";
+//       // Accordion Content
+//       accordionContent[i].setAttribute("aria-hidden", false);
+//       accordionContent[i].style.height = "100%";
+//     } else {
+//       // Accordion Buttons
+//       accordionButtons[i].setAttribute("aria-expanded", false);
+//       accordionButtons[i].children[1].style.display = "block";
+//       // Accordion Content
+//       accordionContent[i].setAttribute("aria-hidden", true);
+//       accordionContent[i].style.height = "";
+//     }
+//   });
+// }
