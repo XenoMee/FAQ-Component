14a8a08661ae109e0ea44f4f2d5e71a873569bca
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-05 13:34:55 +0200

    refactor accordion toggle

--- script.js
+++ script.js
@@ -9,16 +9,12 @@ accordion.addEventListener("click", function (e) {
 function toggleAccordion(panelToActivate) {
   const button = panelToActivate.querySelector(".accordion-trigger");
   const content = panelToActivate.querySelector(".accordion-content");
+  const plusIcon = button.querySelector(".accordion-plus-icon");
+  const isExpanded = button.getAttribute("aria-expanded") === "true";
 
-  if (button.getAttribute("aria-expanded") === "false") {
-    button.setAttribute("aria-expanded", "true");
-    button.children[1].style.display = "none";
-    content.setAttribute("aria-hidden", "false");
-    content.style.height = "100%";
-  } else {
-    button.setAttribute("aria-expanded", "false");
-    button.children[1].style.display = "block";
-    content.setAttribute("aria-hidden", "true");
-    content.style.height = "0%";
-  }
+  button.setAttribute("aria-expanded", String(!isExpanded));
+  content.setAttribute("aria-hidden", String(isExpanded));
+
+  plusIcon.style.display = isExpanded ? "block" : "none";
+  content.style.height = isExpanded ? "0%" : "100%";
 }
