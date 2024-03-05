d3a185c754727e49c344614cd0a4ed7dcce10f62
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-05 14:55:49 +0200

    set default background color and vertical line scale on aria expanded value

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const accordion = document.querySelector(".accordion");
-const accordionIconContainer = document.querySelector(
-  ".accordion-icon-container"
-);
 
 accordion.addEventListener("click", function (e) {
   const activePanel = e.target.closest(".accordion-question");
@@ -12,6 +9,9 @@ accordion.addEventListener("click", function (e) {
 function toggleAccordion(panelToActivate) {
   const button = panelToActivate.querySelector(".accordion-trigger");
   const content = panelToActivate.querySelector(".accordion-content");
+  const accordionIconContainer = button.querySelector(
+    ".accordion-icon-container"
+  );
   const verticalLine = button.querySelector(".vertical-line");
   const isExpanded = button.getAttribute("aria-expanded") === "true";
 
